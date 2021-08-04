59faafaa0b7c110f977fb4de11e9984154f439c6
ISMRITI GUPTA <62712945+Ismriti12@users.noreply.github.com>
2021-08-05 01:54:53 +0530

    Add files via upload

---
+++ contentScript.js
@@ -0,0 +1,21 @@
+chrome.runtime.onMessage.addListener((message, function(){outputSummary(message)} ));
+function outputSummary(message){}
+  if (message == "generate"){
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    console.log(tabs[0].url);
+    var link = tabs[0].url;
+    function callback() {
+    if (oReq.readyState === XMLHttpRequest.DONE) {
+        if (oReq.status === 200) {
+            result = oReq.responseText;
+            chrome.runtime.sendMessage({"message": "result", "data": result});
+        }
+    }
+};
+
+var oReq = new XMLHttpRequest();
+oReq.open("GET", link, true);
+oReq.onreadystatechange = callback;
+oReq.send();;
+});
+}
